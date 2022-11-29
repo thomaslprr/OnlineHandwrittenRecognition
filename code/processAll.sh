4ab0f1eaa9a654d47fa3c86b1f147b72ccbe8695
Thomas Lapierre <thomaslapierre@orange.fr>
2022-11-29 11:11:03 +0100

    exported files

--- code/processAll.sh
+++ code/processAll.sh
@@ -25,27 +25,34 @@ mkdir "$OUTDIR/hyp"
 mkdir "$OUTDIR/seg"
 mkdir "$OUTDIR/symb"
 mkdir "$OUTDIR/out"
+
+cpt = 0
+
 for file in $1/*.inkml
 do
-    echo "Recognize: $file"
-    BNAME=`basename $file .inkml`
-    OUT="$OUTDIR/out/$BNAME.lg"
-    OUT_hyp="$OUTDIR/hyp/$BNAME.lg"
-    OUT_seg="$OUTDIR/seg/$BNAME.lg"
-    OUT_symb="$OUTDIR/symb/$BNAME.lg"
-    python3 ./code/segmenter.py -i $file -o $OUT_hyp 
-    ERR=$? # test de l'erreur au cas où...
-     python3 ./code/segmentSelect.py -o $OUT_seg  $file $OUT_hyp 
-    ERR=$ERR || $?
-     python3 ./code/symbolReco.py  -o $OUT_symb $file $OUT_seg
-    ERR=$ERR || $?
-     python3 ./code/selectBestSeg.py -o $OUT $OUT_symb 
-    ERR=$ERR || $?
-    
-    if [ $ERR -ne 0 ]
-    then 
-        echo "erreur !" $ERR
-        exit 2
+    if [$((cpt)) > 666];
+    then
+        echo "Recognize: $file"
+        BNAME=`basename $file .inkml`
+        OUT="$OUTDIR/out/$BNAME.lg"
+        OUT_hyp="$OUTDIR/hyp/$BNAME.lg"
+        OUT_seg="$OUTDIR/seg/$BNAME.lg"
+        OUT_symb="$OUTDIR/symb/$BNAME.lg"
+        python3 ./code/segmenter.py -i $file -o $OUT_hyp 
+        ERR=$? # test de l'erreur au cas où...
+        python3 ./code/segmentSelect.py -o $OUT_seg  $file $OUT_hyp 
+        ERR=$ERR || $?
+        python3 ./code/symbolReco.py  -o $OUT_symb $file $OUT_seg
+        ERR=$ERR || $?
+        python3 ./code/selectBestSeg.py -o $OUT $OUT_symb 
+        ERR=$ERR || $?
+        
+        if [ $ERR -ne 0 ]
+        then 
+            echo "erreur !" $ERR
+            exit 2
+        fi
     fi
+    ((cpt=cpt+1))
 done
 echo "done."
