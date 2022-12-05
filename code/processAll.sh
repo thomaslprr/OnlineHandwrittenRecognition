3ecd4188de945fe183e9eaf38c2163e5d442ce77
Thomas Lapierre <thomaslapierre@orange.fr>
2022-12-05 18:40:42 +0100

    soft voting with 3 models

--- code/processAll.sh
+++ code/processAll.sh
@@ -31,7 +31,7 @@ mkdir "$OUTDIR/out"
 
 for file in $1/*.inkml
 do
-    if [[ $cpt -gt -1 ]];
+    if [[ $cpt -gt 245 ]];
     then
         echo "Recognize: $file"
         BNAME=`basename $file .inkml`
